106a4839a4ce55aee16d44414d518d179a154f16
Dimitrios Glentadakis <dglent@free.fr>
2014-11-05 06:07:35 +0100

    Update resources file

--- meteo_qt/qrc_resources.py
+++ meteo_qt/qrc_resources.py
@@ -2,7 +2,7 @@
 
 # Resource object code
 #
-# Created: Τρί Νοε 4 22:14:24 2014
+# Created: Τετ Νοε 5 06:06:11 2014
 #      by: The Resource Compiler for PyQt (Qt v4.8.6)
 #
 # WARNING! All changes made in this file will be lost!
