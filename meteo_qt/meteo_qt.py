ff34393986caae0ef6aeef6215aaa3a572c3b78a
Dimitrios Glentadakis <dglent@free.fr>
2015-06-07 20:35:09 +0200

    Logging simplification and Formatter improvement

--- meteo_qt/meteo_qt.py
+++ meteo_qt/meteo_qt.py
@@ -709,28 +709,25 @@ def main():
                       QLibraryInfo.location(QLibraryInfo.TranslationsPath))
     app.installTranslator(qtTranslator)
 
-    log_level = settings.value('Logging_Level')
+    log_level = settings.value('Logging/Level')
     if log_level == '' or log_level == None:
         log_level = 'INFO'
-        settings.setValue('Logging_Level', 'INFO')
-    if log_level == 'INFO':
-        log_level = 20
-    elif log_level == 'DEBUG':
-        log_level = 10
+        settings.setValue('Logging/Level', 'INFO')
 
     log_filename = os.path.dirname(settings.fileName())
     log_filename = log_filename + '/meteo-qt.log'
 
-    logging.basicConfig(format='%(asctime)s %(message)s - %(name)s - %(levelname)s',
-                        datefmt='%m/%d/%Y %I:%M:%S',
+    logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s - %(module)s - %(name)s',
+                        datefmt='%m/%d/%Y %H:%M:%S',
                         filename=log_filename, level=log_level)
-    logger = logging.getLogger('MyLogger')
+    logger = logging.getLogger('meteo-qt')
     logger.setLevel(log_level)
-    handler = logging.handlers.RotatingFileHandler(log_filename, maxBytes=20,
-                                                   backupCount=5)
-
+    handler = logging.handlers.RotatingFileHandler(
+        log_filename, maxBytes=20, backupCount=5)
     logger1 = logging.getLogger()
     handler1 = logging.StreamHandler()
+    logger1Formatter = logging.Formatter('%(levelname)s: %(message)s - %(module)s')
+    handler1.setFormatter(logger1Formatter)
     logger.addHandler(handler)
     logger1.addHandler(handler1)
 
